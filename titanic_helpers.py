3b0894aaf177c840ccf99ec75bb7a3f5d3400020
nirlalum <nirlalum13@gmail.com>
2021-12-17 17:37:19 +0200

    started data modeling
    for now:
    1. dummy classifier
    2. logistic regression

--- titanic_helpers.py
+++ titanic_helpers.py
@@ -1,4 +1,4 @@
-# ===== Packages =====
+# ===== PACKAGES =====
 import pandas as pd
 from pandas.core.frame import DataFrame
 import sklearn as sk
@@ -10,12 +10,16 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 from prettytable import PrettyTable
 
+from sklearn.metrics import accuracy_score, f1_score, fbeta_score, recall_score, precision_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
 
 plt.rcParams['figure.figsize'] = (8.0, 8.0)
 plt.rcParams['axes.grid'] = True
 plt.rcParams['font.size'] = True
 
 
+# ----- FUNCTIONS -----
 def value_counts_all(df: DataFrame):
     """Use pandas value_counts function for each column in the Dataframe
     Parameters
@@ -33,7 +37,7 @@ def value_counts_all(df: DataFrame):
 
 
 def missing_values_table(df_train, df_test):
-    table = PrettyTable(field_names = ['Feature', 'Missing values in train', 'Missing values in test'])
+    table = PrettyTable(field_names = ['Feature', 'Missing values in train', 'Missing values in test'], )
     for feature in df_test.columns:
         table.add_row([feature, len(df_train[df_train[feature].isnull()]), len(df_test[df_test[feature].isnull()])])
     
@@ -83,3 +87,27 @@ def data_split(dataset:DataFrame ,n_splits=1, test_size=0.2, train_size=0.8):
     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
     
     return X_train, y_train, X_test, y_test
+
+
+def model_evaluation(y_true, y_pred):
+    """Evaluate the ML model according to different metrics
+    Parameters
+    ----------
+    y_true: Data structure containing the true labels of the examples
+    y_pred: Data structure containing the prediction of the ML model
+    Returns
+    -------
+    None
+    """
+
+    metric_table = PrettyTable(float_format='.3', field_names = ['Metric', 'Score'])
+    metric_table.add_row(['accuracy', accuracy_score(y_true, y_pred)])
+    metric_table.add_row(['recall', recall_score(y_true, y_pred)])
+    metric_table.add_row(['presicion', precision_score(y_true, y_pred)])
+    
+    print(metric_table)
+
+    disp = ConfusionMatrixDisplay(confusion_matrix(y_true, y_pred))
+    disp.plot()
+    disp.ax_.set_xticklabels(['negative','positive'])
+    disp.ax_.set_yticklabels(['negative','positive'])
